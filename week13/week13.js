1f7cfba1145b5cf5537d347da178c89b69a23c44
丁泳聖 <91170812+yungsheng0825@users.noreply.github.com>
2022-05-19 17:08:43 +0800

    add week13

---
+++ week13/week13.js
@@ -0,0 +1,30 @@
+function setup() {
+    initializeFields();
+    createCanvas(300, 300);
+    fill(color(0x97, 0xC4, 0xFA));
+    textSize(50);
+    textAlign(CENTER, CENTER);
+}
+
+var choice;
+
+function draw() {
+    background(color(0xFF, 0xFA, 0xE8));
+    text("Dinner 1", 150, 50);
+    text("Dinner 2", 150, 150);
+    text("Dinner 3", 150, 250);
+    if (choice == 0)
+        ellipse(30, 50, 50, 50);
+    if (choice == 1)
+        ellipse(30, 150, 50, 50);
+    if (choice == 2)
+        ellipse(30, 250, 50, 50);
+}
+
+function mousePressed() {
+    choice = int(random(3));
+}
+
+function initializeFields() {
+    choice = -1;
+}
